aecdae15a6b598cf6a393ef7e6318276993bb8be
Etienne Curati <etienne.curati@gmail.com>
2018-05-09 17:00:34 -0300

    Test image

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -5,6 +5,7 @@ class ViewsController < ApplicationController
 			view = View.create 
 			view.open_counter += 1
 			view.save
+			redirect_to "https://vagas-programador.herokuapp.com/assets/logo-f55af0183edfbcab4406c4472840ebef327f3464d4d97c4265a9b041e776bfe2.png"
 		end
 
 end
